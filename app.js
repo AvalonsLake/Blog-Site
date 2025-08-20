9e40e8276b523c61348c8d1c9d44947d3f842650
AvalonsLakes <avalonslake@gmail.com>
2025-08-20 12:51:05 -0600

    starting

---
+++ app.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`Server is Listening on Port ${port}...`));
