f7179de7f856ec8812418f5a05f8b8c764c35f8e
AvalonsLake <avalonslake@gmail.com>
2025-08-27 13:00:12 -0600

    functionality update

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
 const expressLayouts = require("express-ejs-layouts");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
+
+const methodOverride = require("method-override");
+const session = require("express-session");
 
 const connectDB = require("./server/config/db");
 connectDB();
@@ -11,11 +16,27 @@ connectDB();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//cookies
+app.use(cookieParser());
+app.use(methodOverride("_method"));
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
+  })
+);
+
 app.use(expressLayouts);
 app.use(express.static("public"));
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
 app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
 
 app.listen(port, () => console.log(`Server is Listening on Port ${port}...`));
